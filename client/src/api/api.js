6c88cc9648d0960f333669d203e1a39a7cd8ae5d
Theeraphat011 <67035011@kmitl.ac.th>
2025-04-21 04:57:09 +0700

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: import.meta.env.REACT_APP_API_URL || "https://authentication-dcvi.onrender.com",
+   baseURL: import.meta.env.REACT_APP_API_URL || "https://authentication-production-1a7c.up.railway.app",
    withCredentials: true,
 });
 
