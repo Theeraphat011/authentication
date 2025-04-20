91bb371a8581e7819f42ef7d13f05645dcca0fac
Theeraphat011 <67035011@kmitl.ac.th>
2025-04-21 05:38:05 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,25 +6,7 @@ const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/authRoutes");
 const app = express();
 
-const allowedOrigins = [
-   "https://auth-jw5ow2j5g-theeraphat011s-projects.vercel.app",
-];
-
-const corsOptions = {
-   origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-         callback(null, true);
-      } else {
-         callback(new Error("Not allowed by CORS"));
-      }
-   },
-   methods: ["GET", "POST"],
-   allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-// ใช้ CORS middleware
 app.use(cors());
-
 app.use(express.json());
 app.use(cookieParser());
 
